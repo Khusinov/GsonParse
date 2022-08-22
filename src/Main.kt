1c61555f608959911cb4375849f94f0eee0f668b
Khusinov <shaxriyorxusinov2020@gmail.com>
2022-08-22 13:34:37 +0500

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+
+import Amaliyotchi
+import com.google.gson.Gson
+import java.io.File
+
+fun main() {
+
+    val gson = Gson()
+    val amaliyotchi = Amaliyotchi()
+    amaliyotchi.name = "Shaxriyor"
+    amaliyotchi.surname = "Xusinov"
+    amaliyotchi.stack = "Android"
+
+    val file = File("amaliyot.txt")
+    val str = gson.toJson(amaliyotchi)
+    val array = str.toByteArray()
+    file.writeBytes(array)
+
+
+    val jsonString = file.readText()
+    gson.fromJson<Amaliyotchi>(jsonString, Amaliyotchi::class.java)
+    println("${amaliyotchi.name} ${amaliyotchi.surname}  ${amaliyotchi.stack}")
+
+}
